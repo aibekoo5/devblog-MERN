35a00996fd5e812411a3be7b0f337f7b25cc3f48
aibekoo5 <kemel.aibek@mail.ru>
2026-05-18 13:33:00 +0500

    frontend jest conf

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,21 +1,15 @@
 const nextJest = require('next/jest');
 
-const createJestConfig = nextJest({
-  // Points to your Next.js app root
-  dir: './',
-});
+const createJestConfig = nextJest({ dir: './' });
 
 const customConfig = {
   testEnvironment: 'jsdom',
-  setupFilesAfterFramework: ['<rootDir>/jest.setup.js'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
     '\\.css$': '<rootDir>/__mocks__/styleMock.js',
   },
   testMatch: ['**/__tests__/**/*.test.jsx'],
-  transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': ['@swc/jest'],
-  },
 };
 
 module.exports = createJestConfig(customConfig);
